ff0ce19f2a76d9c15a2e2712da07a3c851448f09
soumyajit365 <soumyajitdas365@gmail.com>
2023-07-12 18:01:32 +0530

    added icons

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF0A66C2);
+const Color backgroundColor = Color(0xFFE9E5DF);
+const Color darkBackgroundColor = Color(0xFF031A31);
